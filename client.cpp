9799367a2f3ba7ae88725a4d0ff223a480455633
grigoriyaleinik <galeinik0906@gmail.com>
2025-11-07 10:28:28 +0300

    Create client.cpp

---
+++ client.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    const string requestFile = "C:/Users/s0177102/source/repos/file/REQUEST.txt";
+
+    while (true) {
+        string name;
+        int coffeeParams[4];
+
+        cout << "Введите имя клиента: ";
+        cin >> name;
+        cout << "Введите параметры кофе [крепость(1-5) сахар(0-5) молоко(0-3) объем(1-3)]: ";
+        for (int i = 0; i < 4; i++)
+            cin >> coffeeParams[i];
+
+        // Создаём файл клиента
+        string path = "C:/Users/s0177102/source/repos/file/" + name + ".txt";
+        ofstream fout(path);
+        for (int i = 0; i < 4; i++)
+            fout << coffeeParams[i] << " ";
+        fout.close();
+
+        // Добавляем имя в общий файл запросов
+        ofstream foutReq(requestFile, ios::app);
+        foutReq << name << endl;
+        foutReq.close();
+
+        cout << "Заказ отправлен бариста. Ожидание приготовления..." << endl;
+
+        // Ожидаем, пока сервер запишет "Ответ:"
+        string line;
+        bool answered = false;
+        while (!answered) {
+            Sleep(500);
+            ifstream fin(path);
+            while (getline(fin, line)) {
+                if (line.find("Ответ:") != string::npos) {
+                    cout << line << endl;
+                    answered = true;
+                    break;
+                }
+            }
+            fin.close();
+        }
+
+        cout << "----------------------------------\n";
+    }
+
+    return 0;
+}
